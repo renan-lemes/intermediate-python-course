3d74cb0a22ffd847afdb5ffb87d15a35d76bd4a7
renan-lemes <61756319+renan-lemes@users.noreply.github.com>
2021-10-24 14:51:43 -0300

    Tutorial complete

--- dice_roller.py
+++ dice_roller.py
@@ -4,12 +4,18 @@ import random
 def main():
     # roll = 5
     # roll = random.randint(1, 6)
-    dice_rolls = 2
+    dice_rolls = int(input('Quantosdados você gostaria de lançar?'))
     dice_sums = 0
+    dice_size = int(input('Quantos lados são os dados?'))
     for i in range(0, dice_rolls):
-        roll = random.randint(1, 6)
+        roll = random.randint(1, dice_size)
         dice_sums += roll
-        print(f'Você deu um {roll}')
+        if roll == 1:
+            print(f'Você obteve um {roll} : falha critica')
+        elif roll == dice_size:
+            print(f'Voce você obteve um {roll} : Sucesso critico!')
+        else:
+            print(f'Você rolou um {roll}')
     print(f'soma dos dados foi { dice_sums }')
 
 
