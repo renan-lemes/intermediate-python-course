5fc60908aedc0733379b8d29712e1afdcf214828
renan-lemes <61756319+renan-lemes@users.noreply.github.com>
2021-10-24 14:26:19 -0300

    Now rolling two dice

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,16 @@
+import random
+
+
 def main():
-    print('You rolled a die')
+    # roll = 5
+    # roll = random.randint(1, 6)
+    dice_rolls = 2
+    dice_sums = 0
+    for i in range(0, dice_rolls):
+        roll = random.randint(1, 6)
+        dice_sums += roll
+        print(f'Você deu um {roll}')
+    print(f'soma dos dados foi { dice_sums }')
 
 
 if __name__ == "__main__":
